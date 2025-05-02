2225cc1f17e8f43397b91639ab671f35cd86cf61
joaohenriquefernandes <joaohenriquefernandes.dev@gmail.com>
2025-05-02 17:12:35 -0300

    feat: implement authN and protected routes

--- src/modules/auth/auth.service.ts
+++ src/modules/auth/auth.service.ts
@@ -1,8 +1,13 @@
-import { Injectable, UnauthorizedException } from '@nestjs/common';
+import {
+  ConflictException,
+  Injectable,
+  UnauthorizedException,
+} from '@nestjs/common';
 import { JwtService } from '@nestjs/jwt';
-import { compare } from 'bcryptjs';
+import { compare, hash } from 'bcryptjs';
 import { UsersRepository } from 'src/shared/database/repositories/users.repositories';
-import { AuthenticateDto } from './dto/authenticate.dto';
+import { SigninDto } from './dto/signin.dto';
+import { SignupDto } from './dto/signup.dto';
 
 @Injectable()
 export class AuthService {
@@ -11,7 +16,44 @@ export class AuthService {
     private readonly jwtService: JwtService,
   ) {}
 
-  async authenticate({ email, password }: AuthenticateDto) {
+  async signup({ email, name, password }: SignupDto) {
+    const emailTaken = await this.usersRepository.findUnique({
+      where: { email },
+      select: { id: true },
+    });
+    if (emailTaken)
+      throw new ConflictException('This email is already in use.');
+    const hashedPassword = await hash(password, 10);
+    const user = await this.usersRepository.create({
+      data: {
+        email,
+        name,
+        password: hashedPassword,
+        categories: {
+          createMany: {
+            data: [
+              { name: 'Salário', icon: 'travel', type: 'INCOME' },
+              { name: 'Freelance', icon: 'freelance', type: 'INCOME' },
+              { name: 'Outro', icon: 'other', type: 'INCOME' },
+              { name: 'Casa', icon: 'home', type: 'EXPENSE' },
+              { name: 'Alimentação', icon: 'food', type: 'EXPENSE' },
+              { name: 'Educação', icon: 'education', type: 'EXPENSE' },
+              { name: 'Lazer', icon: 'fun', type: 'EXPENSE' },
+              { name: 'Mercado', icon: 'grocery', type: 'EXPENSE' },
+              { name: 'Roupas', icon: 'clothes', type: 'EXPENSE' },
+              { name: 'Transporte', icon: 'transport', type: 'EXPENSE' },
+              { name: 'Viagem', icon: 'travel', type: 'EXPENSE' },
+              { name: 'Outro', icon: 'other', type: 'EXPENSE' },
+            ],
+          },
+        },
+      },
+    });
+    const accessToken = await this.generateAccessToken(user.id);
+    return { accessToken };
+  }
+
+  async signin({ email, password }: SigninDto) {
     const user = await this.usersRepository.findUnique({
       where: { email },
     });
@@ -19,7 +61,11 @@ export class AuthService {
     const isPasswordMatch = await compare(password, user.password);
     if (!isPasswordMatch)
       throw new UnauthorizedException('Invalid credentials.');
-    const accessToken = await this.jwtService.signAsync({ sub: user.id });
+    const accessToken = await this.generateAccessToken(user.id);
     return { accessToken };
+  }
+
+  private async generateAccessToken(userId: string) {
+    return this.jwtService.signAsync({ sub: userId });
   }
 }
--- src/modules/users/users.service.ts
+++ src/modules/users/users.service.ts
@@ -1,45 +1,11 @@
-import { ConflictException, Injectable } from '@nestjs/common';
-import { hash } from 'bcryptjs';
+import { Injectable } from '@nestjs/common';
 import { UsersRepository } from 'src/shared/database/repositories/users.repositories';
-import { CreateUserDto } from './dto/create-user.dto';
 
 @Injectable()
 export class UsersService {
   constructor(private readonly usersRepository: UsersRepository) {}
 
-  async create({ email, name, password }: CreateUserDto) {
-    const emailTaken = await this.usersRepository.findUnique({
-      where: { email },
-      select: { id: true },
-    });
-    if (emailTaken)
-      throw new ConflictException('This email is already in use.');
-    const hashedPassword = await hash(password, 10);
-    const user = await this.usersRepository.create({
-      data: {
-        email,
-        name,
-        password: hashedPassword,
-        categories: {
-          createMany: {
-            data: [
-              { name: 'Salário', icon: 'travel', type: 'INCOME' },
-              { name: 'Freelance', icon: 'freelance', type: 'INCOME' },
-              { name: 'Outro', icon: 'other', type: 'INCOME' },
-              { name: 'Casa', icon: 'home', type: 'EXPENSE' },
-              { name: 'Alimentação', icon: 'food', type: 'EXPENSE' },
-              { name: 'Educação', icon: 'education', type: 'EXPENSE' },
-              { name: 'Lazer', icon: 'fun', type: 'EXPENSE' },
-              { name: 'Mercado', icon: 'grocery', type: 'EXPENSE' },
-              { name: 'Roupas', icon: 'clothes', type: 'EXPENSE' },
-              { name: 'Transporte', icon: 'transport', type: 'EXPENSE' },
-              { name: 'Viagem', icon: 'travel', type: 'EXPENSE' },
-              { name: 'Outro', icon: 'other', type: 'EXPENSE' },
-            ],
-          },
-        },
-      },
-    });
-    return user;
+  async getUserById(userId: string) {
+    console.log(userId);
   }
 }
