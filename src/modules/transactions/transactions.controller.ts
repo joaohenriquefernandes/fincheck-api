2bb1cbe6f38e60da147c806785ca39a8e18e19f6
joaohenriquefernandes <joaohenriquefernandes.dev@gmail.com>
2025-05-08 12:00:38 -0300

    feat: create query filters and custom pipes

--- src/modules/transactions/transactions.controller.ts
+++ src/modules/transactions/transactions.controller.ts
@@ -6,14 +6,19 @@ import {
   HttpCode,
   HttpStatus,
   Param,
+  ParseIntPipe,
   ParseUUIDPipe,
   Post,
   Put,
+  Query,
 } from '@nestjs/common';
 import { ActiveUserId } from 'src/shared/decorators/ActiveUserId';
+import { OptionalParseEnumPipe } from 'src/shared/pipes/OptionalParseEnumPipe';
+import { OptionalParseUUIDPipe } from 'src/shared/pipes/OptionalParseUUIDPipe';
 import { CreateTransactionDto } from './dto/create-transaction.dto';
 import { UpdateTransactionDto } from './dto/update-transaction.dto';
-import { TransactionsService } from './entities/services/transactions.service';
+import { TransactionType } from './entities/Transaction';
+import { TransactionsService } from './services/transactions.service';
 
 @Controller('transactions')
 export class TransactionsController {
@@ -28,8 +33,21 @@ export class TransactionsController {
   }
 
   @Get()
-  findAll(@ActiveUserId() userId: string) {
-    return this.transactionsService.findAllByUserId(userId);
+  findAll(
+    @ActiveUserId() userId: string,
+    @Query('month', ParseIntPipe) month: number,
+    @Query('year', ParseIntPipe) year: number,
+    @Query('bankAccountId', OptionalParseUUIDPipe)
+    bankAccountId?: string,
+    @Query('type', new OptionalParseEnumPipe(TransactionType))
+    type?: TransactionType,
+  ) {
+    return this.transactionsService.findAllByUserId(userId, {
+      month,
+      year,
+      bankAccountId,
+      type,
+    });
   }
 
   @Get(':transactionId')
---
+++ src/shared/pipes/OptionalParseUUIDPipe.ts
@@ -0,0 +1,13 @@
+import { ArgumentMetadata, ParseUUIDPipe } from '@nestjs/common';
+
+export class OptionalParseUUIDPipe extends ParseUUIDPipe {
+  override async transform(
+    value: string,
+    metadata: ArgumentMetadata,
+  ): Promise<any> {
+    if (typeof value === 'undefined') {
+      return undefined;
+    }
+    return super.transform(value, metadata);
+  }
+}
